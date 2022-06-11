582bbb08e0d19fd8d1eb2e200dbb2008ea7d3016
originalzzzelectric <zzzelectric@gmail.com>
2022-06-11 09:09:11 -0500

    solution to returning the frequency of most frequent element in array

---
+++ CC-06-11-2022/app.js
@@ -0,0 +1,9 @@
+// Complete the function to find the count of the most frequent item of an array. You can assume that input is an array of integers. For an empty array return 0
+
+// Example
+// input array: [3, -1, -1, -1, 2, 3, -1, 3, -1, 2, 4, 9, 3]
+// ouptut: 5 
+//params: an array of integersd
+//return: for an empty array return 0, otherwise, return the count of the most frequent item
+//example: [3, -1, -1, -1, 2, 3, -1, 3, -1, 2, 4, 9, 3] return 5
+//
